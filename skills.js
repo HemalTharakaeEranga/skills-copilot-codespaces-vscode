5682843f71e9b004acab02fd99cfdf43a2910348
Hemal Tharaka Eranaga <156353444+HemalTharakaeEranga@users.noreply.github.com>
2024-06-06 19:20:00 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,9 @@
+function calculateNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+//typescript use this below code
+
+/*function calculateNumbers(var1: number, var2: number): number {
+    return var1 + var2;
+}*/
