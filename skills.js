cc859624f38e3c56863748d3551328106029c613
Hemal Tharaka Eranaga <156353444+HemalTharakaeEranga@users.noreply.github.com>
2024-06-06 19:26:48 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,9 +1,5 @@
 function calculateNumbers(var1, var2) {
-    return var1 + var2;
+    console.log(var1 + var2);
 }
 
-//typescript use this below code
 
-/*function calculateNumbers(var1: number, var2: number): number {
-    return var1 + var2;
-}*/
